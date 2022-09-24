cb5c6ecd758043b5057e5eeb751999d6735e1e57
SAYAH Imad <i_sayah@esi.dz>
2022-09-24 01:25:30 -0400

    pattern builder

---
+++ design-pattern/Creational/design-pattern.builder/Builder.cs
@@ -0,0 +1,20 @@
+﻿namespace design_pattern.builder
+{
+    internal class Builder : IBuilder
+    {
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+
+        public IBuilder SetDescription(string description)
+        {
+            this.Description = description;
+            return this;
+        }
+
+        public IBuilder SetName(string name)
+        {
+            this.Name = name;
+            return this;
+        }
+    }
+}
---
+++ design-pattern/Creational/design-pattern.builder/IBuilder.cs
@@ -0,0 +1,8 @@
+﻿namespace design_pattern.builder
+{
+    internal interface IBuilder
+    {
+        IBuilder SetName(string name);
+        IBuilder SetDescription(string description);
+    }
+}
